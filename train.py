3030c49fe81e64922f12d1a2d00ec141e4e63e60
0-keun <dudrms7352@naver.com>
2025-06-12 17:40:28 +0900

    _

--- train.py
+++ train.py
@@ -8,12 +8,12 @@ from tensorflow.keras.callbacks import EarlyStopping
 from datetime import date, datetime
 from utils.data_processing import normalizae_and_save
 from utils.models import FFNN_model
-from utils.utils import load_json, save_acc_plot, save_loss_plot, name_date, name_time
-
-MODEL_DIR = './model/'+name_date(default_name='model')+'/'
+from utils.utils import load_json, save_acc_plot, save_loss_plot, name_date, name_time, name_to_dir
 
 p = load_json('./params.json')
 df = pd.read_csv(p.train_data_dir)
+
+MODEL_DIR = name_to_dir(name='model',time_flag=True)
 
 X = df[p.feature_list].values 
 y = df[p.output_list].values  
@@ -23,7 +23,6 @@ X = scaler.transform(X)
 
 model = FFNN_model(feature_num=len(p.feature_list), output_num=len(p.output_list))
 
-# --- 5. 모델 학습 ---
 history = model.fit(
     X, y,
     epochs=p.epochs,
@@ -31,10 +30,9 @@ history = model.fit(
     validation_split=0.2
 )
 
-save_loss_plot(history=history,loss_filepath='loss.png',time_flag=True)
+save_loss_plot(history=history,loss_filename='loss.png',time_flag=True)
 
-# --- 7. 모델 저장 ---
-model_name = name_time('DNN_DAB_est','.h5')
+model_name = name_time('DNN_DAB_est.h5')
 os.makedirs(MODEL_DIR, exist_ok=True)
 model_path = os.path.join(MODEL_DIR, model_name)
 model.save(model_path)
